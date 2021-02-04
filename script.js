73dca8eeaafda283b8047c97fe32d237d3700b93
Horex <hosseinhorex@gmail.com>
2021-02-04 17:10:30 +0330

    user introduction created

---
+++ script.js
@@ -0,0 +1,8 @@
+let userFirstName;
+let userLastName;
+
+userFirstName = prompt("Hellooo whats your name ?");
+
+userLastName = prompt("dear " + userFirstName  + " whats your last name?");
+
+alert("Nice to meet you " + userFirstName + " " + userLastName +" " + ", welcome to my website");
